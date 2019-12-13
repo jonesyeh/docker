b26de9cf1fbc35ceb4be8f11252b4692c92745f8
jones yeh <jonesyeh@msn.com>
2019-12-13 10:02:04 +0800

    新增多參數替換欄位函數

--- lab.sql
+++ lab.sql
@@ -1,5 +1,5 @@
 USE [master]
-declare @i int=1,@count int=2
+declare @i int=1,@j int=1,@r int=3 ,@c int=4
 
 declare @db_name varchar(255)='SBP'
 DECLARE @backup_file nvarchar(255)=N'C:\mssql\SBP.bak'
@@ -18,14 +18,7 @@ Begin
 	Select @SqlStatement = 'CREATE LOGIN sbp_user with password= '''+@sbp_user_pwd +''';ALTER SERVER ROLE [sysadmin] ADD MEMBER [sbp_user]'	
     EXEC sp_executesql @SqlStatement	
 end 
-while(@i<=@count)
-begin
 begin try 
-     set @seq=replace(str(@i,2,0),' ','0');
-	 set @db_name='LAB'+@seq
-	 set @mdb_file=N'C:\mssql\LAB'+@seq+'.mdf'
-	 set @log_file=N'C:\mssql\LAB'+@seq+'_log.ldf'
-	
 	IF EXISTS (SELECT 1 FROM SYS.DATABASES WHERE NAME = @db_name) 
 	begin
 		set @detach_statement='BEGIN EXEC sp_detach_db '+@db_name+' END;'
@@ -45,6 +38,40 @@ begin catch
 	RECOVERY,  NOUNLOAD,  REPLACE,  STATS = 5
 end catch
 Select @SqlStatement = 'USE '+@db_name +';ALTER USER sbp_user WITH LOGIN=sbp_user'	
-    EXEC sp_executesql @SqlStatement	
-set @i=@i+1
+    EXEC sp_executesql @SqlStatement
+while(@i<=@r)
+begin
+	while(@j<=@c)
+	begin
+		begin try 
+			 
+			 set @seq=replace(str(@i*10+@j,2,0),' ','0');
+			 set @db_name='LAB'+@seq
+			 set @mdb_file=N'C:\mssql\LAB'+@seq+'.mdf'
+			 set @log_file=N'C:\mssql\LAB'+@seq+'_log.ldf'
+			
+			IF EXISTS (SELECT 1 FROM SYS.DATABASES WHERE NAME = @db_name) 
+			begin
+				set @detach_statement='BEGIN EXEC sp_detach_db '+@db_name+' END;'
+				EXECUTE sp_executesql @detach_statement
+			end
+			set @attach_statement='	CREATE DATABASE '+@db_name+' ON 
+				( FILENAME = '''+ @mdb_file+''' ),
+				( FILENAME = '''+@log_file+''' )
+				 FOR ATTACH'
+				 EXECUTE sp_executesql @attach_statement
+
+		end try
+		begin catch
+			RESTORE DATABASE @db_name FROM  DISK = @backup_file WITH  FILE = 1,
+			MOVE @data_name TO @mdb_file,
+			MOVE @log_name TO @log_file, 
+			RECOVERY,  NOUNLOAD,  REPLACE,  STATS = 5
+		end catch
+		Select @SqlStatement = 'USE '+@db_name +';ALTER USER sbp_user WITH LOGIN=sbp_user'	
+			EXEC sp_executesql @SqlStatement	
+		set @j=@j+1
+	end
+	set @i=@i+1
+	set @j=1
 end
